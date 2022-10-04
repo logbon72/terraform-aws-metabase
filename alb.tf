2d6537374339967d4a27f4a81ebb5cb94c3adefb
Taiwo Orilogbon <taiwo@nestcoin.com>
2022-10-04 14:05:45 -0400

    Update * Completed fixes for deprecation warnings.

--- alb.tf
+++ alb.tf
@@ -58,30 +58,38 @@ resource "aws_lb_listener" "https" {
   }
 }
 
+
 resource "aws_s3_bucket" "this" {
   bucket_prefix = "mb-"
-  acl           = "private"
-  force_destroy = ! var.protection
+  force_destroy = !var.protection
   tags          = var.tags
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+  lifecycle {
+    create_before_destroy = true
   }
+}
 
-  lifecycle_rule {
-    enabled = true
-
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    id     = "mb-bucket-rule"
+    status = "Enabled"
     expiration {
       days = var.log_retention
     }
   }
+}
 
-  lifecycle {
-    create_before_destroy = true
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
